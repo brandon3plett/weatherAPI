b0dc714a7e3ccdd985f6d320ae4bb741a893e37e
brandon3plett <btriplett216757@gmail.com>
2019-03-01 15:00:08 -0500

    Added weather tab icon, added comments in app.js

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# weatherAPI
-
 This weather API grabs the user's geolocation(with permission), and displays the current weather condition.
 
 Technologies used:
@@ -11,4 +9,4 @@ For the API, I used https://openweathermap.org
 
 The current temperature unit is fahrenheit.  To switch to celsius, just click on the unit displayed.
 
-I will try to update this with the ability to display a 5 day weather forecast for the current location.
+You can only grab the current weather temperature and condition in your location at that particular time.  In the future, I would like to display a 5 day forecast.
--- assets/js/app.js
+++ assets/js/app.js
@@ -13,30 +13,40 @@ $(document).ready(() => {
     let src = "http://openweathermap.org/img/w/"
 
     currentWeather = (() => {
+        //Used to pinpoint current location on device, will ask for permission first
         navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude
-            let lon = position.coords.longitude
+            let lat = position.coords.latitude //Get latitude coordinates and store in variable
+            let lon = position.coords.longitude //Get longitude coordinates and store in variable
+            
+            //For the queryURL, we had to set the default unit to fahrenheit(imperial)
+            //The default unit is kelvin when first making the request
             let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey
 
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then((data) => {
-                console.log(data)
+                //To get the results, we pass in data as a parameter
+                //console.log(data)
                 $("#city").text(data.name + ", ")
                 $("#country").text(data.sys.country)
                 mainTemp.text(Math.round(data.main.temp) + " " + String.fromCharCode(176))
                 tempUnit.text(defaultUnit)
+
+                //Click function that handles switching the displayed unit from fahrenheit to celsius and vice versa
                 tempUnit.click(() => {
                     let currentTempUnit = tempUnit.text()
                     let newTempUnit = currentTempUnit == "F" ? "C" : "F"
+
                     tempUnit.text(newTempUnit)
+
                     if (newTempUnit == "C") {
                         mainTemp.text(convertToC(Math.round(data.main.temp)) + " " + String.fromCharCode(176))
                     } else {
                         mainTemp.text(Math.round(data.main.temp) + " " + String.fromCharCode(176))
                     }
                 })
+
                 $("#condition").text(data.weather[0].main)
                 $("#icon").attr("src", src + data.weather[0].icon + ".png")
 
